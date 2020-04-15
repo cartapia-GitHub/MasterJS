d7d5517659c33b2c45645b268f821d3a793bc43c
Carolina Tapia <cartapia@gmail.com>
2020-04-15 19:09:12 -0400

    actualizacion de archivos

--- hola-mundo/js/06-condicionales.js
+++ hola-mundo/js/06-condicionales.js
@@ -1,13 +1,58 @@
 'use strict'
 
 //condicional if
-var edad1= 30;
-var edad2=12;
+var edad= 74;
+var nombre = 'David Suarez';
 
-if(edad1>edad2){
-   console.log("Edad1 es mayor que edad2");
+/* 
+//Operadores relacionales
+Mayor: >
+Menor: <
+Mayor o Igual: >=
+Menor o Igual: <=
+Igual: ==
+Distinto: !=
+*/
+
+if(edad>=18){
+   console.log(nombre+" tiene "+edad+" años, es mayor de edad " );
+   if(edad<=33){
+   		console.log("Todavía eres Millenial");
+   }
+   else if(edad >=70){
+   		console.log("Eres Anciano");
+   }
+   else{
+   		console.log("Ya no eres Millenial");
+   }
 }
 else{
-	console.log("Edad2 es mayor que edad1");
+	console.log(nombre+" tiene "+edad+" años, es menor de edad");
 }
 
+/*
+//Operadores Lógicos
+AND(Y): &&
+OR(O): ||
+Negación: !
+*/
+
+//Negación
+var year = 2028;
+if(year!=2016){
+	console.log("El año no es 2016, realmente es: "+year);
+}
+//AND
+if(year >= 2000 && year<=2020 && year!=2018){
+	console.log("Estamos en la era actual");
+}
+else{
+	console.log("Estamos en la era post moderna");
+}
+//OR
+if(year == 2008 || (year >= 2018 && year==2028)){
+	console.log("El año acaba en 8");
+}
+else{
+	console.log("AÑO NO REGISTRADO");
+}
