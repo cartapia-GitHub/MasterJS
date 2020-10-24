d5a75b2bf10e2374ee1124fde89da6dc49f12961
Carolina <cartapia@gmail.com>
2020-10-24 17:01:23 -0300

    added efectos

--- hola-mundo/Aprendiendo-JQuery/js/04-efectos.js
+++ hola-mundo/Aprendiendo-JQuery/js/04-efectos.js
@@ -1,13 +1,48 @@
 $(document).ready(function(){
+	var caja = $("#caja");
 	$("#mostrar").hide();
 	$("#mostrar").click(function(){
 		$(this).hide();
 		$("#ocultar").show();
-		$("#caja").show();
+		caja.slideDown('slow');
 	});
 	$("#ocultar").click(function(){
 		$(this).hide();
 		$("#mostrar").show();
-		$("#caja").hide();
+		caja.slideUp('slow', function(){
+			console.log("Cartel Ocultado");
+		});
+		//.show(), .show('slow'),.show('normal'),.show('fast')
+		//.fadeIn(),.fadeOut(), .fadeTo('slow',0.8), .fadeTo('slow',0.2)
+		// .fadeTo('slow',1), .fadeTo('slow',0)
+	});
+	$("#todoenuno").click(function(){
+		caja.slideToggle('fast');
+		//toggle,fadeToggle,slideToggle
+	});
+	$("#animar").click(function(){
+		caja.animate({
+			marginLeft: '500px',
+			fontSize:'40px',
+			height:'110px'
+		}, 'slow')
+		.animate({
+			borderRadius: '900px',
+			marginTop: '80px'
+		},'slow')
+		.animate({
+			borderRadius: '0px',
+			marginLeft: '0px'
+		},'slow')
+		.animate({
+			borderRadius: '100px',
+			marginTop: '0px'
+		},'slow')
+		.animate({
+			marginLeft: '500px',
+			fontSize:'40px',
+			height:'110px'
+		}, 'slow')
+		;
 	});
 });
---
+++ hola-mundo/Aprendiendo-JQuery/js/05-ajax.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	//Load
+	//$("#datos").load("https://reqres.in/");
+
+	//Get y Post
+	$.get("https://reqres.in/api/users",{page: 2}, function(response){
+		response.data.forEach((element, index) =>{
+			$("#datos").append("<p>"+element.first_name+" "+element.last_name+"</p>");
+		});
+	});
+
+	
+
+	$("#formulario").submit(function(e){
+		e.preventDefault();
+
+		var usuario = {
+			name:$('input[name="name"]').val(),
+			web:$('input[name="web"]').val()
+		}
+		console.log(usuario);
+
+		$.post($(this).attr("action"), usuario,function(response){
+			console.log(response);
+		}).done(function(){
+			alert("Usuario añadido correctamente");
+		});
+
+		return false;
+
+	});
+});
